8d12cc66bdb5cf0024c74d253e3c132c690b866e
Hung Bui <hungbui7690@gmail.com>
2023-03-01 23:53:58 -0700

    Abstraction

--- app.js
+++ app.js
@@ -1,36 +1,23 @@
 /*
-  ENUMERATING PROPERTIES
-  - Iterating means repeating some steps > similar to looping
-  - enumerating means going through all values in a collection of values
-  - loop is a sequence of instruction s that is continually repeated until a certain condition is reached
-
+  ABSTRACTION
+  - hide the details
+  - show the essentials
 */
 
+// essential = radius + draw()
 function Circle(radius) {
   this.radius = radius
+  this.defaultLocation = { x: 0, y: 0 }
+  this.computeLocation = function () {
+    console.log('compute location')
+  }
+
+  // draw() call computeLocation()
   this.draw = function () {
+    this.computeLocation()
     console.log('draw')
   }
 }
+
 const circle = new Circle(10)
-
-// Enumerating
-// typeof key = string/number, typeof circle[key] = number/function
-for (let key in circle) {
-  console.log(key, circle[key], typeof circle[key])
-}
-
-// Convert keys to array
-const keys = Object.keys(circle)
-console.log(keys)
-
-// Convert keys to array
-const values = Object.values(circle)
-console.log(values)
-
-// Array of arrays
-const entries = Object.entries(circle)
-console.log(entries)
-
-// check if key exists in object or not
-if ('radius' in circle) console.log('Circle has a radius')
+circle.draw()
