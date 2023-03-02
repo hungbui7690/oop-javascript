acc340430bbfb6da2bee811edfe799741ec2f42d
Hung Bui <hungbui7690@gmail.com>
2023-03-01 22:47:25 -0700

    Factories

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+/*
+  FACTORY FUNCTIONS
+
+*/
+
+// Object Literals
 const circle = {
   radius: 1,
   location: {
@@ -9,4 +15,16 @@ const circle = {
   },
 }
 
-circle.draw()
+/////////////////////////////////
+// Factories
+function createCircle(radius) {
+  return {
+    radius: 1,
+    draw: function () {
+      console.log('draw')
+    },
+  }
+}
+
+const circleX = createCircle(1)
+circleX.draw()
