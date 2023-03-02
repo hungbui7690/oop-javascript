3b8e28fb22acd08f5cafb173267a7e3cd761c1a3
Hung Bui <hungbui7690@gmail.com>
2023-03-01 23:58:43 -0700

    Private Properties & Methods

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 /*
-  ABSTRACTION
-  - hide the details
-  - show the essentials
+  PRIVATE PROPERTIES & METHODS
+  
 */
 
-// essential = radius + draw()
 function Circle(radius) {
+  // this is not a property of this object, it just a local variable of this function > though we cannot access this variable outside of this object
+  let color = 'red'
+
   this.radius = radius
-  this.defaultLocation = { x: 0, y: 0 }
-  this.computeLocation = function () {
+
+  // this also a local function to this function
+  let computeLocation = function () {
     console.log('compute location')
   }
 
-  // draw() call computeLocation()
   this.draw = function () {
-    this.computeLocation()
+    console.log(color)
+    computeLocation()
     console.log('draw')
   }
 }
-
 const circle = new Circle(10)
+
+// we cannot call computeLocation() & get color property here since they are scoped to Circle function only
 circle.draw()
