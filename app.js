e59289456a18085586b9341f037dfc547ec3fc07
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 17:06:14 -0600

    Private Properties using Symbol P3

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 /*
-  Private Properties using Symbol P2
-  - there are 3 approaches to make the property become private
-    + Method 1: use _
-      > super terrible
+  Private Properties using Symbol P3
+  + Method 2: use Symbol()
+
 
 */
 
 class Circle {
   constructor(radius) {
-    this._radius = radius // (***)
-    // this['_radius'] = radius // 2nd way to access property
+    this._radius = radius
   }
 
   draw() {
@@ -18,5 +16,7 @@ class Circle {
 }
 
 const circle = new Circle(10)
-circle._radius = 100 // though we use _, we still can access it outside > because it is just convention between developers > it does not prevent dev to change the property
 console.log(circle)
+
+// (***)
+console.log(Symbol() === Symbol()) // false
