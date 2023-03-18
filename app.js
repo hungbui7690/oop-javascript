008637f1965b6c8e5adae0438182120c5d2c9e14
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 13:08:20 -0600

    Prototype vs Instance Members P1

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 /*
-  Constructor Prototypes P4
+  Prototype vs Instance Members P1
   
 */
 
+// Circle has 2 members
 function Circle(radius) {
   this.radius = radius
+
+  this.draw = function () {
+    console.log('draw')
+  }
 }
 
-const circle = new Circle(10)
-console.log(circle) // (***)
-
-console.log(Circle.prototype) // (***) this is the same as prototype of "circle" object
+// (***) when we check, we will see each object has draw() method > if we have 1000 copies object, we will have 1000 copies of draw() method > next lesson
+const circle1 = new Circle(10)
+const circle2 = new Circle(20)
