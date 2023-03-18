70fda888c55ae74e33a3ae545896685562104aff
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 14:12:47 -0600

    Create Your Own Prototypical Inheritance P1

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 /*
+  Create Your Own Prototypical Inheritance P1
+  - why
 
 */
+
+function Circle(radius) {
+  this.radius = radius
+}
+
+Circle.prototype.draw = function () {
+  console.log('draw')
+}
+
+Circle.prototype.duplicate = function () {
+  console.log('duplicate')
+}
+
+// (***)
+function Square() {}
+
+// (***) we don't want to do this
+Square.prototype.duplicate = function () {}
