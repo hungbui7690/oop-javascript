3521ab0c06d4b8f6fd3f8d4fee503864f77462db
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 13:54:49 -0600

    Solution

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 /*
-  Exercise:
-  - move start(), stop(), reset() to prototype
+  Solution:
+  - similar to this, we need to make those local variables to become read-only property
+    > we pollute the interface of our class > since before, we just have 3 functions + 1 property > also validate the idea behind abstraction
 
-  (***) when working with objects that have the same type > we should move function to prototype > otherwise, don't use this as the optimal technique > put these methods in the prototype will lead to some issue (learn later)
 */
 
 function StopWatch() {
@@ -11,33 +11,60 @@ function StopWatch() {
     running = false,
     duration = 0
 
-  this.start = function () {
-    if (running) throw new Error('Stopwatch has already started')
-
-    running = true
-    startTime = new Date()
-  }
-
-  this.stop = function () {
-    if (!running) throw new Error('Stopwatch has already stopped')
-
-    running = false
-    endTime = new Date()
-    duration = (endTime - startTime) / 1000
-  }
-
-  this.reset = function () {
-    duration = 0
-    startTime = null
-    endTime = null
-    running = false
-  }
-
+  // (***) create getter and setter for these properties
   Object.defineProperty(this, 'duration', {
     get: function () {
       return duration
     },
+    set: function (value) {
+      duration = value
+    },
   })
+  Object.defineProperty(this, 'startTime', {
+    get: function () {
+      return startTime
+    },
+    set: function (value) {
+      startTime = value
+    },
+  })
+  Object.defineProperty(this, 'endTime', {
+    get: function () {
+      return endTime
+    },
+    set: function (value) {
+      endTime = value
+    },
+  })
+  Object.defineProperty(this, 'running', {
+    get: function () {
+      return running
+    },
+    set: function (value) {
+      running = value
+    },
+  })
+}
+
+// (***)
+StopWatch.prototype.start = function () {
+  if (this.running) throw new Error('Stopwatch has already started')
+
+  this.running = true
+  this.startTime = new Date()
+}
+StopWatch.prototype.stop = function () {
+  if (!this.running) throw new Error('Stopwatch has already stopped')
+
+  this.running = false
+  this.endTime = new Date()
+  this.duration = (this.endTime - this.startTime) / 1000
+}
+StopWatch.prototype.reset = function () {
+  this.duration = 0
+  this.startTime = null
+  this.endTime = null
+  this.running = false
 }
 
 const sw = new StopWatch()
