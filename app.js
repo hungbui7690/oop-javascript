f68ca5981cbba528a204abc3cf2ff04c6d9fd7a8
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 14:25:24 -0600

    Create Your Own Prototypical Inheritance P3

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 /*
-  Create Your Own Prototypical Inheritance P2
-  - below: we have this:
-    + Object Base > Shape Base > s 
-    + Object Base > Circle Base > c
-
-  - we want to implement this: 
-    + Object Base > Shape Base > Circle Base > c 
-      > we will use Prototypical Inheritance > next lecture
+  Create Your Own Prototypical Inheritance P3
+  - implement: Object Base > Shape Base > Circle Base > c 
+  
+  - Before, we need to do these 2 steps, now we just need to do 1 step
+    > Circle.prototype = Object.create(Object.prototype) // this will be done implicitly
+    > Circle.prototype = Object.create(Shape.prototype)
 
 */
 
-// (***)
 function Shape() {}
-
 Shape.prototype.duplicate = function () {
   console.log('duplicate')
 }
 
-const s = new Shape()
+//////////////////////////////////////
 
-// (***)
 function Circle(radius) {
   this.radius = radius
 }
 
+// (***)
+Circle.prototype = Object.create(Shape.prototype)
+
+// (***) this must be placed below inheritance step
 Circle.prototype.draw = function () {
   console.log('draw')
 }
+
 const c = new Circle(10)
+console.log(c) // we will see the object tree that we want
+c.duplicate() // now, we can access duplicate()
+c.draw()
