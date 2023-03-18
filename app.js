7c0cb436a59958379aa5287754e0832a993acdeb
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 13:28:32 -0600

    Avoid Extending Built-in Objects

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
 /*
-  Prototype vs Instance Members P7
-  
+  Avoid Extending Built-in Objects
+  - we should not modify built-in methods 
+    + in below example, we add shuffle() > if in the later version, they add shuffle() > bug
+    + another reason, if we modify built-in method (like toString()) > and the framework we are using uses that toString() method in the framework > bug
+
+  (***) don't modify objects that we don't own 
 */
 
-function Circle(radius) {
-  this.radius = radius
-
-  this.draw = function () {
-    console.log('draw')
-  }
+Array.prototype.shuffle = function () {
+  console.log('shuffle')
 }
 
-Circle.prototype.move = function () {
-  console.log('move')
-}
-
-const circle = new Circle(10)
-circle.move()
-
-// (***) hasOwnProperty() return instance members > that's why in some document: instance property === own property
-console.log(circle.hasOwnProperty('radius')) // true
-console.log(circle.hasOwnProperty('draw')) // true
-console.log(circle.hasOwnProperty('move')) // false
+const arr = []
+console.log(arr.shuffle())
