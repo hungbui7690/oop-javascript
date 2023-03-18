6257d8bef7096a41bb17697af1b978131d6413a9
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 16:29:20 -0600

    ES6 Classes P1

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 /*
-
+  ES6 Classes P1
+  
 */
+
+// b4 ES6
+function Circle(radius) {
+  this.radius = radius
+
+  this.draw = function () {
+    console.log('draw')
+  }
+}
+
+const circle = new Circle(10)
+console.log(circle)
