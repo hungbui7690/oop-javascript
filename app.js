535e08a9732035c6d9260b1a7b243afd8b3df334
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 13:34:36 -0600

    Exercise

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 /*
-  Avoid Extending Built-in Objects
-  - we should not modify built-in methods 
-    + in below example, we add shuffle() > if in the later version, they add shuffle() > bug
-    + another reason, if we modify built-in method (like toString()) > and the framework we are using uses that toString() method in the framework > bug
+  Exercise:
+  - move start(), stop(), reset() to prototype
 
-  (***) don't modify objects that we don't own 
+  (***) when working with objects that have the same type > we should move function to prototype > otherwise, don't use this as the optimal technique > put these methods in the prototype will lead to some issue (learn later)
 */
 
-Array.prototype.shuffle = function () {
-  console.log('shuffle')
+function StopWatch() {
+  let startTime,
+    endTime,
+    running = false,
+    duration = 0
+
+  this.start = function () {
+    if (running) throw new Error('Stopwatch has already started')
+
+    running = true
+    startTime = new Date()
+  }
+
+  this.stop = function () {
+    if (!running) throw new Error('Stopwatch has already stopped')
+
+    running = false
+    endTime = new Date()
+    duration = (endTime - startTime) / 1000
+  }
+
+  this.reset = function () {
+    duration = 0
+    startTime = null
+    endTime = null
+    running = false
+  }
+
+  Object.defineProperty(this, 'duration', {
+    get: function () {
+      return duration
+    },
+  })
 }
 
-const arr = []
-console.log(arr.shuffle())
+const sw = new StopWatch()
