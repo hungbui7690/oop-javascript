c1cd298c9ee39eaa98d7f9b119410fab26469a98
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 14:59:16 -0600

    Calling the Super Constructor P2

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 /*
-  Calling the Super Constructor P1
+  Calling the Super Constructor P2
 
 */
 
-// (***) add color property
 function Shape(color) {
   this.color = color
 }
@@ -14,7 +13,10 @@ Shape.prototype.duplicate = function () {
 
 ////////////////////////////////////////////
 
-function Circle(radius) {
+// (***) we need to add "color" property to Circle() as well
+function Circle(radius, color) {
+  Shape(color)
+
   this.radius = radius
 }
 
@@ -26,6 +28,7 @@ Circle.prototype.draw = function () {
   console.log('draw')
 }
 
-// (***) now we create the circle > we don't see the "color" property, but we see only the "radius" property
-const c1 = new Circle(10)
+// (***) though we do this, we still don't see "red" > reason: because we use new keyword, it creates the {}, then point this keyword to Circle, but not for Shape: this of Shape will point to window object > we can verify: window.color
+const c1 = new Circle(10, 'red')
 console.log(c1)
+console.log(window.color) // red
