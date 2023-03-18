6b3c7fb9e722acfc647137fad60f53fc851d021d
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 17:12:10 -0600

    Private Properties using Symbol P4

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 /*
-  Private Properties using Symbol P3
+  Private Properties using Symbol P4
   + Method 2: use Symbol()
 
 
 */
 
+const _radius = Symbol()
+
 class Circle {
   constructor(radius) {
-    this._radius = radius
+    this[_radius] = radius
+    // this._radius = radius // (***)  don't use this, otherwise, we still can change the original _radius
   }
 
   draw() {
@@ -16,7 +19,7 @@ class Circle {
 }
 
 const circle = new Circle(10)
+// circle._radius = 10000 // though we change _radius here, we actually did't change the real _radius > when we do this here, we create new _radius property in the class
 console.log(circle)
 
-// (***)
-console.log(Symbol() === Symbol()) // false
+console.log(Object.getOwnPropertyNames(circle)) // []
