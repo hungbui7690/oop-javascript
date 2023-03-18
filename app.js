9cb4d74c1051f324a6ccdae111bb8e254d02e6dd
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 13:15:14 -0600

    Prototype vs Instance Members P4

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
 /*
-  Prototype vs Instance Members P3
+  Prototype vs Instance Members P4
   
 */
 
 function Circle(radius) {
   this.radius = radius
 
-  // Instance Member
   this.draw = function () {
     console.log('draw')
   }
 }
 
-// Prototype Member
 Circle.prototype.move = function () {
   console.log('move')
+  this.draw() // (***) access instance member from prototype
 }
 
 const circle = new Circle(10)
-console.log(circle)
-
-// (***)
-console.log(circle.toString())
-
-// (***) override toString()
-Circle.prototype.toString = function () {
-  return `This Circle has radius of ${this.radius}`
-}
-console.log(circle.toString())
+circle.move() // (***)
