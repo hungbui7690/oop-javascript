ef6d5ef7359345f4fbdd39953f96e442f7393379
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 16:33:28 -0600

    ES6 Classes P3

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 /*
-  ES6 Classes P2
+  ES6 Classes P3
   
 */
 
 class Circle {
   constructor(radius) {
     this.radius = radius
+
+    // (***) if we want method to be in Circle, but not in prototype > put in constructor
+    this.move = function () {
+      console.log('move')
+    }
   }
 
-  // (***)
   draw() {
     console.log('draw')
   }
 }
 
+// (***) with ES6 classes, if we don't have "new" keyword > err (unlike we use ES5 constructor function, add to window object)
 const circle = new Circle(10)
-console.log(circle) // (***) we can see that draw() is in prototype
+console.log(circle)
