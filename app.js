9074ff4ff335ab32dca6bdc7203aed9eb5b041c5
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 17:31:30 -0600

    Private Properties using WeakMap P2

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 /*
-  Private Properties using WeakMap P1
+  Private Properties using WeakMap P2
   + Method 3: WeakMap()
-    - technically, we still can access to this private property if we have access to WeakMap 
-    - this can be solve easily when we use modules to prevent access later
+
 */
 
-const _radius = new WeakMap() // (***)
+const _radius = new WeakMap()
 
 class Circle {
   constructor(radius) {
-    _radius.set(this, radius) // (***)
+    _radius.set(this, radius)
+  }
+
+  log() {
+    console.log(_radius.get(this)) // (***) the only way we can access private property
   }
 }
 
 const circle = new Circle(10)
-circle._radius = 99 // create new property
-
 console.log(circle)
+
+circle.log() // (***)
