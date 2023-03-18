3920480ed1db849e430e65b66bb71c9dcb145ff1
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 14:28:40 -0600

    Resetting the Constructor P1

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 /*
-  Create Your Own Prototypical Inheritance P3
-  - implement: Object Base > Shape Base > Circle Base > c 
-  
-  - Before, we need to do these 2 steps, now we just need to do 1 step
-    > Circle.prototype = Object.create(Object.prototype) // this will be done implicitly
-    > Circle.prototype = Object.create(Shape.prototype)
+  Resetting the Constructor P1
 
 */
 
@@ -13,21 +8,16 @@ Shape.prototype.duplicate = function () {
   console.log('duplicate')
 }
 
-//////////////////////////////////////
-
 function Circle(radius) {
   this.radius = radius
 }
 
-// (***)
-Circle.prototype = Object.create(Shape.prototype)
+// (***) comment this line
+// Circle.prototype = Object.create(Shape.prototype)
 
-// (***) this must be placed below inheritance step
 Circle.prototype.draw = function () {
   console.log('draw')
 }
 
 const c = new Circle(10)
-console.log(c) // we will see the object tree that we want
-c.duplicate() // now, we can access duplicate()
-c.draw()
+console.log(c) // (***) check prototype > we can see constructor()
