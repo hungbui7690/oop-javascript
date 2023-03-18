58264a6851103b55b606e5972e3619f1b5837b10
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 17:54:00 -0600

    Private Properties using WeakMap P3

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 /*
-  Private Properties using WeakMap P2
+  Private Properties using WeakMap P3
   + Method 3: WeakMap()
 
 */
 
 const _radius = new WeakMap()
 
+const _move = new WeakMap() // (***) private method
+
 class Circle {
   constructor(radius) {
     _radius.set(this, radius)
+
+    // (***) need to be in constructor function
+    _move.set(this, function () {
+      console.log('move', this)
+    })
   }
 
   log() {
-    console.log(_radius.get(this)) // (***) the only way we can access private property
+    _move.get(this)() // (***)
+    console.log(_radius.get(this)) // undefined since class is run in strict mode
   }
 }
 
