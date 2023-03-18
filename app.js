060b4dfd8a244499eeefaeb536645f1e0412544f
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 12:45:50 -0600

    Property Attributes P5

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
-  Property Attributes P4
+  Property Attributes P5
 
 */
 
@@ -7,12 +7,9 @@ let person = { name: 'Joe' }
 
 Object.defineProperty(person, 'name', {
   writable: false,
-  enumerable: false, // (***) cannot iterate
+  enumerable: false,
+  configurable: true, // (***) make this property deletable
 })
 
-// (***) return nothing
-for (const key in person) console.log(key)
-
-// (***)
-const keys = Object.keys(person)
-console.log(keys) // []
+delete person.name // delete property
+console.log(person) // {}
