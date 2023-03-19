6665a1ba17dda5946dec428963297ec049a3f60c
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 18:14:07 -0600

    Method Overriding P2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
-  Method Overriding P1
+  Method Overriding P2
 
 */
 
@@ -9,7 +9,7 @@ class Shape {
   }
 
   move() {
-    console.log('move')
+    console.log('Shape Moves')
   }
 }
 
@@ -23,12 +23,12 @@ class Circle extends Shape {
     console.log('draw')
   }
 
-  // (***)
   move() {
+    super.move() // (***) call move() from parent
     console.log('Circle Moves')
   }
 }
 
 const circle = new Circle(10, 'red')
-console.log(circle) // (***) check this
+console.log(circle)
 circle.move()
