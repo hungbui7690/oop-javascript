ee2817601e0c42963d0a0b7f547c0c00492c8c56
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 18:05:53 -0600

    Getters & Setters P2

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 /*
-  Getters & Setters P1
-  -
+  Getters & Setters P2
 
 */
 
@@ -8,15 +7,22 @@ const _radius = new WeakMap()
 
 class Circle {
   constructor(radius) {
-    _radius: _radius.set(this, radius) // (***)
+    _radius: _radius.set(this, radius)
   }
 
   getRadius() {
-    return _radius.get(this) // (***) earlier, we use Object.defineProperty() > but we should not use that we > since it pollutes our code
+    return _radius.get(this)
+  }
+
+  // (***)
+  setRadius(value) {
+    if (value <= 0) throw new Error('Invalid radius!!!')
+
+    _radius.set(this, value)
   }
 }
 
 const circle = new Circle(10)
 
-// (***)
+circle.setRadius(20) // (***)
 console.log(circle.getRadius())
