ba644a41368425a316278ff0c4cbd0074d87e3e9
hungbui7690 <hungbui7690@gmail.com>
2023-03-18 18:25:25 -0600

    Exercise: Stack

--- app.js
+++ app.js
@@ -1,34 +1,52 @@
 /*
-  Method Overriding P2
+  Exercise
+  - create a Stack class which has these: 
+    + push()
+    + pop()  > throw error when Stack is empty
+    + count()
+    + peek()  > throw error when Stack is empty
 
+
+  (***) use WeakMap() to make the list of items in the stack private
 */
 
-class Shape {
-  constructor(color) {
-    this.color = color
+const _items = new WeakMap()
+
+class Stack {
+  constructor() {
+    _items.set(this, [])
   }
 
-  move() {
-    console.log('Shape Moves')
+  push(value) {
+    _items.get(this).push(value)
+  }
+
+  pop() {
+    if (_items.get(this).length === 0) throw new Error('Stack is empty')
+    return _items.get(this).pop()
+  }
+
+  peek() {
+    if (_items.get(this).length === 0) throw new Error('Stack is empty')
+
+    const items = _items.get(this)
+    return items[items.length - 1]
+  }
+
+  getCount() {
+    return _items.get(this).length
   }
 }
 
-class Circle extends Shape {
-  constructor(radius, color) {
-    super(color)
-    this.radius = radius
-  }
-
-  draw() {
-    console.log('draw')
-  }
-
-  move() {
-    super.move() // (***) call move() from parent
-    console.log('Circle Moves')
-  }
-}
-
-const circle = new Circle(10, 'red')
-console.log(circle)
-circle.move()
+const stack = new Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+console.log(stack.pop()) // 4
+console.log(stack.pop()) // 3
+console.log(stack.peek()) // 2
+console.log(stack.pop()) // 2
+console.log(stack.pop()) // 1
+// stack.pop() // error
+// stack.peek() // error
